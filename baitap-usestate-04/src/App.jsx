64a626598be156845690f26be5a84dd04fae03ae
Phạm Công Vinh <pcv.fed@gmail.com>
2025-05-06 15:46:16 +0700

    Bài tập 04 - Hook[useState] - bài tập vè Bật/Tắt đèn

---
+++ baitap-usestate-04/src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css';
+import ToogleBtn from './components/ToogleBtn/ToogleBtn';
+
+function App() {
+    return (
+        <ToogleBtn/>
+    );
+}
+
+export default App
---
+++ baitap-usestate-04/src/components/ToogleBtn/ToogleBtn.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+const light = 'https://www.w3schools.com/js/pic_bulbon.gif';
+const dark = 'https://www.w3schools.com/js/pic_bulboff.gif';
+function ToogleBtn(props) {
+    const [isOn,setIsOn] = useState(false);
+    const handleBtn = () => {
+        setIsOn(!isOn);
+    }
+    
+    return (
+        <div>
+            <img src={isOn ? light : dark} alt="Bóng đèn" />
+            <h1>{isOn ? 'Bóng đèn đang bật' : 'Bóng đèn đang tắt'}</h1>
+            <button onClick={handleBtn}>
+                {isOn ? 'Tắt đèn' : 'Bật đèn'}
+            </button>
+        </div>
+    );
+}
+
+export default ToogleBtn;
